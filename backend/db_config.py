277bcec510b606ca23a1e808515ecf899eab4e7c
hasu621 <calingal621@gmail.com>
2025-12-06 18:26:12 +0800

    Cloud Database

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,7 +1,15 @@
-# db_config.py
+import os
+from dotenv import load_dotenv
+
+# Load the secret variables from the .env file
+load_dotenv()
+
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'capstone',
-    'password': 'CLLLSTBSIT4A_2526',  # <--- PALITAN MO ITO BFF!
-    'database': 'frames_database'
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),       # Cloud DBs use special ports (not 3306)
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'), 
+    'database': os.getenv('DB_NAME'),   # This will be 'defaultdb'
+    'ssl_ca': 'ca.pem',                 # The certificate file you downloaded earlier
+    'ssl_disabled': False
 }
